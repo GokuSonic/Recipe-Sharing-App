708056bf59c0ea3a97d360695c6b17ff96f2295a
sean8499 <sean8499@gmail.com>
2019-11-12 16:54:32 -0800

    DB message,recpie

--- app/src/main/java/com/example/ReciPleaseLogin/data/DB.java
+++ app/src/main/java/com/example/ReciPleaseLogin/data/DB.java
@@ -2,6 +2,7 @@ package com.example.ReciPleaseLogin.data;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 
@@ -24,7 +25,11 @@ public class DB {
             db.collection("users").document(user.getUid()).set(object);
         }
         else if (object instanceof Recipe) {
-            db.collection("users").document(user.getUid()).collection("recipes").add(object);
+            if (((Recipe) object).premium==true)
+                db.collection("PremiumRecipes").add(object);
+            else if (((Recipe) object).premium==false){
+                db.collection("PublicRecipes").add(object);
+            }
         }
         else if (object instanceof Message){
             db.collection("users").document(user.getUid()).collection("messages").add(object);
@@ -43,6 +48,47 @@ public class DB {
         return object;
     };
 
+
+
+
+    static public  void query(query query ){
+        String collection=query.collection;
+
+        CollectionReference ref=db.collection(collection);
+
+        /*(< , <= , ==, > >=, array-contains, in, array-contains-any "*/
+        for (int i=0;i<query.num_queries;i++){
+            if (query.type[i]=="<") {
+                ref.whereLessThan(collection, query.searchfor[i]);
+            }
+            else if (query.type[i]=="<=") {
+                ref.whereLessThanOrEqualTo(collection, query.searchfor[i]);
+            }
+            else if (query.type[i]=="==") {
+                ref.whereEqualTo(collection, query.searchfor[i]);
+            }
+            else if (query.type[i]==">") {
+                ref.whereGreaterThan(collection, query.searchfor[i]);
+            }
+            else if (query.type[i]==">=") {
+                ref.whereGreaterThanOrEqualTo(collection, query.searchfor[i]);
+            }
+            else if (query.type[i]=="array-contains") {
+                ref.whereArrayContains(collection, query.searchfor[i]);
+            }
+            //not found
+            //else if (query.type[i]=="in") {
+              //  ref.w(collection, query.searchfor[i]);
+    //        }
+  //          else if (query.type[i]=="array-contains-any") {
+//                ref.whereEqualTo(collection, query.searchfor[i]);
+      //      }
+        }
+
+
+
+
+    }
 
 
 }
---
+++ app/src/main/java/com/example/ReciPleaseLogin/data/User.java
@@ -0,0 +1,16 @@
+package com.example.ReciPleaseLogin.data;
+
+public class User {
+    int instance=0;
+    UserFeed userFeed;
+    UserProfile CurrentProfile;
+
+
+private void GetInstance(){
+    if (instance==0)
+        instance=1;
+}
+
+
+
+}

28c202dd0468fba739e47456cb9a3c53bf40cbd3
Patrick Caldwell <41460848+GokuSonic@users.noreply.github.com>
2019-12-05 01:40:32 -0800

    all done except premium
                     -Patrick Caldwell
    
    Signed-off-by: Patrick Caldwell <41460848+GokuSonic@users.noreply.github.com>

--- app/src/main/java/com/example/ReciPleaseLogin/ui/Levels/LevelsActivity.java
+++ app/src/main/java/com/example/ReciPleaseLogin/ui/Levels/LevelsActivity.java
@@ -4,11 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.ReciPleaseLogin.R;
+import com.example.ReciPleaseLogin.data.DB;
 import com.example.ReciPleaseLogin.ui.Edit_Profile.EditProfile;
 import com.example.ReciPleaseLogin.ui.Menu.MenuActivity;
 import com.example.ReciPleaseLogin.ui.Messages.MessagesActivity;
@@ -16,6 +21,13 @@ import com.example.ReciPleaseLogin.ui.Post.PostActivity;
 import com.example.ReciPleaseLogin.ui.PremiumStatus.PremiumActivity;
 import com.example.ReciPleaseLogin.ui.Profile.ProfileActivity;
 import com.example.ReciPleaseLogin.ui.Search.SearchActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class LevelsActivity extends AppCompatActivity {
 
@@ -29,6 +41,7 @@ public class LevelsActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.menu_toolbar_layout);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false); // hide the current title from the Toolbar
+        level();
     }
 
     @Override
@@ -36,6 +49,54 @@ public class LevelsActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+
+    public void level() {
+        Object ReturnInsideOutside = new Object();
+        DatabaseReference dref;
+        dref = FirebaseDatabase.getInstance().getReference("Root").child("users").child(DB.mUser.getUid());
+        dref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                Map<String, Long> myUser = (Map<String, Long>) dataSnapshot.getValue();
+
+                int level = myUser.get("num_likers").intValue();
+
+                ProgressBar progress = findViewById(R.id.cookProgress);
+                RatingBar rate = findViewById(R.id.cookStar);
+                TextView title = findViewById(R.id.cook_level_text);
+                if (level < 1000) {
+                    progress.setMax(1000);
+                    progress.setProgress(level);
+                    title.setText("Cook");
+                    rate.setRating(0);
+                } else if (level >= 1000 && level < 10000) {
+                    progress.setMax(10000);
+                    progress.setProgress(level);
+                    title.setText("Sous Chef");
+                    rate.setRating(1);
+                } else if (level >= 10000 && level < 100000) {
+                    progress.setMax(100000);
+                    progress.setProgress(level);
+                    title.setText("Executive Chef");
+                    rate.setRating(2);
+                } else {
+                    progress.setMax(1000000);
+                    progress.setProgress(level);
+                    rate.setRating(3);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- app/src/main/java/com/example/ReciPleaseLogin/ui/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/ReciPleaseLogin/ui/Profile/ProfileActivity.java
@@ -4,11 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.ReciPleaseLogin.R;
+import com.example.ReciPleaseLogin.data.DB;
+import com.example.ReciPleaseLogin.data.ImageLoadTask;
+import com.example.ReciPleaseLogin.data.Recipe;
+import com.example.ReciPleaseLogin.data.UserProfile;
 import com.example.ReciPleaseLogin.ui.Edit_Profile.EditProfile;
 import com.example.ReciPleaseLogin.ui.Levels.LevelsActivity;
 import com.example.ReciPleaseLogin.ui.Menu.MenuActivity;
@@ -16,6 +24,14 @@ import com.example.ReciPleaseLogin.ui.Messages.MessagesActivity;
 import com.example.ReciPleaseLogin.ui.Post.PostActivity;
 import com.example.ReciPleaseLogin.ui.PremiumStatus.PremiumActivity;
 import com.example.ReciPleaseLogin.ui.Search.SearchActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 
 public class ProfileActivity extends AppCompatActivity {
@@ -30,6 +46,85 @@ public class ProfileActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.menu_toolbar_layout);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false); // hide the current title from the Toolbar
+
+        DatabaseReference dref;
+        dref = FirebaseDatabase.getInstance().getReference("Root").child("users").child(DB.mUser.getUid());
+        dref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Map<String, UserProfile> entry = (Map<String, UserProfile>) dataSnapshot.getValue();
+
+                Map singleUser = (Map) dataSnapshot.getValue();
+
+
+                ImageView image = findViewById(R.id.profile_picture);
+                new ImageLoadTask((String) singleUser.get("pic"), image).execute();
+
+                TextView name = findViewById(R.id.NameTextBox);
+                name.setText((String) singleUser.get("who_are_you"));
+
+                TextView premium = findViewById(R.id.Premium_box);
+                if (!((boolean) singleUser.get("premium"))) {
+                    premium.setText("No Premium");
+                } else {
+                    premium.setText("Premium User");
+                }
+
+                RatingBar rate = findViewById(R.id.ratingStar);
+                int level = ((Long) singleUser.get("num_likers")).intValue();
+                if (level < 1000) {
+                    rate.setRating(0);
+                } else if (level >= 1000 && level < 10000) {
+                    rate.setRating(1);
+                } else if (level >= 10000 && level < 100000) {
+                    rate.setRating(2);
+                } else {
+                    rate.setRating(3);
+                }
+
+                TextView about = findViewById(R.id.AboutMe);
+                about.setText((String) singleUser.get("AboutMe"));
+
+                TextView FB = findViewById(R.id.FBurl);
+                FB.setText("FB:" + singleUser.get("FB"));
+
+                TextView YT = findViewById(R.id.YTurl);
+                YT.setText("YT:" + singleUser.get("YT"));
+
+                TextView IG = findViewById(R.id.IGurl);
+                IG.setText("IG: " + singleUser.get("IG"));
+
+                TextView stats = findViewById(R.id.Likes_and_shares);
+                stats.setText("Likes: " + singleUser.get("num_likers").toString()
+                        + "\nFollowers: " + singleUser.get("num_followers").toString()
+                        + "\nRecipes: " + singleUser.get("num_recipes").toString());
+
+                final DatabaseReference dref = DB.getInstance().database.getReference().child("Root").child("Recipes").child("Public");
+                dref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Recipe myRecipe = findMyRecipe((Map<String, Recipe>) dataSnapshot.getValue());
+
+                        ImageView rec_image = findViewById(R.id.recipe_picture);
+                        new ImageLoadTask(myRecipe.instruction_pics.get(0), rec_image).execute();
+
+                        TextView rec_text = findViewById(R.id.recipe_text);
+                        rec_text.setText("test");
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
     }
 
     @Override
@@ -84,4 +179,26 @@ public class ProfileActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private Recipe findMyRecipe(Map<String, Recipe> recipes) {
+
+        ArrayList<Recipe> recipeList = new ArrayList<>();
+
+        //iterate through each recipe, ignoring their UID
+        for (Map.Entry<String, Recipe> entry : recipes.entrySet()) {
+
+            //Get recipe map
+            Map singleRecipe = (Map) entry.getValue();
+            //Get description field and append to list
+            if (((String) singleRecipe.get("owner")).contentEquals(DB.mUser.getUid())) {
+                return new Recipe(
+                        (String) singleRecipe.get("owner"), (String) singleRecipe.get("posted")
+                        , (String) singleRecipe.get("diet"), (String) singleRecipe.get("recipe_name")
+                        , (String) singleRecipe.get("description"), ((ArrayList) (singleRecipe.get("instruction_pics"))));
+            }
+
+        }
+        return null;
+    }
+
 }
